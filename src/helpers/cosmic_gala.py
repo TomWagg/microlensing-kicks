60652f8348fcb7b35dfa60f1d25c827a10dc0538
Tom Wagg <twagg@college.harvard.edu>
2022-03-11 13:21:11 -0800

    add beginnings of the main evolution function

--- src/helpers/cosmic_gala.py
+++ src/helpers/cosmic_gala.py
@@ -61,3 +61,50 @@ def final_systemic_velocity(v_sys_init, delta_v_sys_xyz, m_1, m_2, a):
                        + delta_v_sys_xyz[2] * np.cos(phi)] * u.km / u.s
 
     return v_sys_init + delta_v_sys_RTz
+
+
+def evolve_binaries_in_galaxy(bpp, kick_info, galaxy_model=None,
+                              galactic_potential=gala.potential.MilkyWayPotential(),
+                              max_ev_time=13.7 * u.Gyr, dispersion=5 * u.km / u.s):
+    # work out how many binaries we are going to evolve
+    bin_nums = bpp["bin_num"].unique()
+    n_bin = len(bin_nums)
+
+    vel_units = u.km / u.s
+
+    # draw random positions and birth times in the galaxy
+    # TODO: actually make this change based on the `galaxy model`
+    lookback_time = draw_lookback_times(n_bin, tm=12 * u.Gyr, tsfr=6.8 * u.Gyr, component="low_alpha_disc")
+    scale_length = R_exp(lookback_time, alpha=0.4)
+    rho = draw_radii(n_bin, R_0=scale_length)
+    scale_height = 0.3 * u.kpc
+    z = draw_heights(n_bin, z_d=scale_height)
+    phi = np.random.uniform(0, 2 * np.pi, size=n_bin) * u.rad
+
+    # calculate the Galactic circular velocity at the given positions
+    x, y = rho * np.cos(phi), rho * np.sin(phi)
+    v_circ = gala.potential.MilkyWayPotential().circular_velocity(q=[x, y, z]).to(vel_units)
+
+    # add some velocity dispersion
+    v_R, v_T, v_z = np.random.normal([np.zeros_like(v_circ), v_circ, np.zeros_like(v_circ)],
+                                     dispersion.to(vel_units) / np.sqrt(3), size=(3, n_bin))
+    v_R, v_T, v_z = v_R * vel_units, v_T * vel_units, v_z * vel_units
+
+    # turn the drawn coordinates into an astropy representation
+    rep = coords.CylindricalRepresentation(rho, phi, z)
+
+    # create differentials based on the velocities (dimensionless angles allows radians conversion)
+    with u.set_enabled_equivalencies(u.dimensionless_angles()):
+        dif = coords.CylindricalDifferential(v_R, (v_T / rho).to(u.rad / u.Gyr), v_z)
+
+    # combine the representation and differentials into a Gala PhaseSpacePosition
+    w0s = gala.dynamics.PhaseSpacePosition(rep.with_differentials(dif))
+
+    # evolve the orbits from birth until present day
+    # TODO: make this actually account for kicks haha (baby steps)
+    orbits = []
+    for bin_num in bin_nums:
+        orbits.append(galactic_potential.integrate_orbit(w0s[bin_num], t1=lookback_time[bin_num],
+                                                         t2=max_ev_time, dt=1 * u.Myr))
+
+    return orbits
